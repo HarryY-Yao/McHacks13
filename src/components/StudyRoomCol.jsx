31b1e2a20e162bab1ee0654474e0f970d7480cc9
Harry Yao <184984708+HarryY-Yao@users.noreply.github.com>
2026-01-17 20:13:05 -0500

    create room/join room button

--- src/components/StudyRoomCol.jsx
+++ src/components/StudyRoomCol.jsx
@@ -4,6 +4,9 @@ export default function StudyRoomCol() {
   return (
     <div className="bg-white h-full">
       <h2>Study Rooms</h2>
+      <button className="bg-red-600 duration-200 ease-in-out hover:bg-red-800 text-white px-8.5 py-2 font-bold rounded-md">
+        + Join/Create Room
+      </button>
     </div>
   );
 }
